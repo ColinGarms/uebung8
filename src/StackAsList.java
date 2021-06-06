33db7290abd4ed340b9fb1db5a4e3cfd0b7c85b0
Maximilian Ehlert <Maximilian.Ehlert@Student.HTW-Berlin.de>
2021-06-06 11:07:16 +0200

    stack now has a toString method

--- src/StackAsList.java
+++ src/StackAsList.java
@@ -7,17 +7,20 @@ public class StackAsList<T> implements Stack<T>{
 		newNode(elem);
 	}
 	
-	public T pull() {
+	public T pop() {
+		if(isEmpty()) throw new 
 		T elem= first.element;
 		first=first.next;
 		return elem;
 	}
-	public boolean isEmptly() {
+	public boolean isEmpty() {
 		if (first.equals(null))return true;
 		else return false;
 	}
 	public T peek() {
-		return first.element;
+		if (first!=null)return first.element;
+		else return null;
+		
 	}
 	
 	public void newNode(T elem) {
@@ -34,4 +37,15 @@ public class StackAsList<T> implements Stack<T>{
 		Node next;
 	}
 	
+
+	public String toString() {
+		Node node=first;
+		String string="";
+		while(node!=null) string+=node.element+" ";
+		return string;
+	}	
+
+	public Node getNext(Node node) {
+		return node.next;
+	}
 }
