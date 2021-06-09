87bd53775da250206d846e067f8d39ab4f62f94c
Maximilian Ehlert <Maximilian.Ehlert@Student.HTW-Berlin.de>
2021-06-09 17:48:11 +0200

    StackAsList class

--- StackAsList.java
+++ StackAsList.java
@@ -1,18 +1,27 @@
+import java.util.EmptyStackException;
 
 public class StackAsList<T> implements Stack<T>{
 
 	Node first;
+
+
+	
 	
 	public void push(T elem) {
 		newNode(elem);
 	}
 	
-	public T pop() {
-		if(isEmpty()) throw new 
+	public T pop() throws EmptyStackException {
+		if(isEmpty()) throw new EmptyStackException();
 		T elem= first.element;
 		first=first.next;
 		return elem;
 	}
+	//well...., look into stack
+	//public T pop1() {return pop();}
+	
+	
+	
 	public boolean isEmpty() {
 		if (first.equals(null))return true;
 		else return false;
